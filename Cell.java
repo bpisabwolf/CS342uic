2a2977d39a5a6103dd667e42853fb82804f3580c
Boris P <32775983+dlkwolf@users.noreply.github.com>
2017-10-30 12:16:14 -0500

    Create Cell.java

---
+++ Cell.java
@@ -0,0 +1,28 @@
+
+public class Cell {
+
+	int xPos, yPos, region, col, row, index;
+	public Cell(int x, int y,int r,int cl, int rw, int i){
+		xPos = x; 
+		yPos = y;
+		region = r;
+		col = cl;
+		row = rw;
+		index = i;
+	}
+	public Cell(){
+		xPos = 0;
+		yPos = 0;
+		region = 0;
+		col = 0;
+		row = 0;
+		
+	}
+	public void determineButtonInfo(){
+		
+	}
+	public void determineArrayInfo(){
+		
+	}
+
+}
