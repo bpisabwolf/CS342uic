2ef07141dc3ac6a7f7f8856cec4e1f3496ac852c
Boris P <32775983+dlkwolf@users.noreply.github.com>
2017-10-30 12:20:35 -0500

    Create SudokuGUIV2.java

---
+++ SudokuGUIV2.java
@@ -0,0 +1,346 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class SudokuGUIV2 extends JFrame implements ActionListener {
+	private Container largeContainer, smallContainer, gridContainer;
+	private GridLayout grid, miniGrid;
+	
+	private MyJButton gridButtons[];
+	private JPanel mainGrid[];
+	private JPanel gridPanel;
+	private JPanel bar;
+	
+	private boolean toggle = true;
+	private String placeHolders[] = {"1", "2", "3", "4", "5", "6","7","8","9"};
+	
+	
+	
+	public SudokuGUIV2(){
+		bar = new SideBar();
+		grid = new GridLayout(3, 3, 5, 5);
+		miniGrid = new GridLayout(3, 3, 0, 0);
+		largeContainer = getContentPane();
+		largeContainer.setLayout(new BorderLayout());
+		largeContainer.add(BorderLayout.EAST, bar);
+
+		
+		gridPanel = new JPanel(grid);
+		
+		largeContainer.add(gridPanel, BorderLayout.CENTER);
+		gridButtons = new MyJButton[9];
+		mainGrid = new MyJPanel[9];
+		
+		for(int i = 0; i < 9; i++)
+		{
+			
+			mainGrid[i] = new MyJPanel(miniGrid, i+1);
+			
+			smallContainer = mainGrid[i];
+			smallContainer.setLayout(miniGrid);
+	
+			gridPanel.add(mainGrid[i]);
+			for(int j = 0; j < gridButtons.length; j++){
+				gridButtons[j] = new MyJButton(placeHolders[j]);
+				gridButtons[j].setNumber(j);
+				gridButtons[j].addActionListener(this);
+				smallContainer.add(gridButtons[j]);				
+			}
+			
+		}
+		setSize(500,500);
+		setVisible( true );
+		
+		
+		JMenu fileMenu = new JMenu("File");
+		fileMenu.setMnemonic( 'F' );
+
+		JMenuItem saveItem = new JMenuItem( "Save puzzle" );
+	    saveItem.setMnemonic( 'S' );
+	    fileMenu.add( saveItem );
+	    saveItem.addActionListener(
+
+	    		new ActionListener() {
+
+	    			public void actionPerformed( ActionEvent event )
+		            {
+		               //save file action to be placed here
+		            }
+	    		}
+	    );
+		
+		
+	    JMenuItem loadItem = new JMenuItem( "Load puzzle..." );
+	    loadItem.setMnemonic( 'L' );
+	    fileMenu.add( loadItem );
+	    loadItem.addActionListener(
+
+	    		new ActionListener() {
+
+	    			public void actionPerformed( ActionEvent event )
+		            {
+		               //load file action to be placed here
+		            }
+	    		}
+	    );
+	    
+
+	    JMenuItem exitItem = new JMenuItem( "Exit" );
+	    exitItem.setMnemonic( 'x' );
+	    fileMenu.add( exitItem );
+	    exitItem.addActionListener(
+
+	    		new ActionListener() {
+
+	    			public void actionPerformed( ActionEvent event )
+	    			{
+	    				System.exit( 0 );
+	    			}
+	    		}
+	    );
+	    
+	    
+	    JMenu helpMenu = new JMenu("Help");
+		helpMenu.setMnemonic( 'H' );
+		
+		JMenuItem how2play = new JMenuItem( "How to play Sudoku" );
+	    how2play.setMnemonic( 'S' );
+	    helpMenu.add( how2play );
+	    how2play.addActionListener(
+
+	    		new ActionListener() {
+
+	    			public void actionPerformed( ActionEvent event )
+	    			{
+	    				JOptionPane.showMessageDialog( SudokuGUIV2.this,
+	    		        "This is how you play Sudoku...",
+	    		        "How to play", JOptionPane.PLAIN_MESSAGE );
+	    			}
+	    		}
+	    );
+	    
+	    
+	    JMenuItem how2interface = new JMenuItem( "How to use the interface" );
+	    how2interface.setMnemonic( 'i' );
+	    helpMenu.add( how2interface );
+	    how2interface.addActionListener(
+
+	    		new ActionListener() {
+
+	    			public void actionPerformed( ActionEvent event )
+	    			{
+	    				JOptionPane.showMessageDialog( SudokuGUIV2.this,
+	    	    		"This is how you use the interface...",
+	    	    		"How to use", JOptionPane.PLAIN_MESSAGE );
+	    			}
+	    		}
+	    );
+	    
+	    JMenuItem aboutItem = new JMenuItem( "About..." );
+	    aboutItem.setMnemonic( 'A' );
+	    helpMenu.add( aboutItem );
+	    aboutItem.addActionListener(
+
+	    		new ActionListener() {
+
+	    			public void actionPerformed( ActionEvent event )
+	    			{
+	    				JOptionPane.showMessageDialog( SudokuGUIV2.this,
+	    	    		"Robert Barrera - rbarre4 \nBoris Pisabaj - bpisab3 \nDavid Qiao - dqiao4",
+	    	    		"Creators of this program", JOptionPane.PLAIN_MESSAGE );
+	    			}
+	    		}
+	    );
+	    
+	    
+	    JMenu hintMenu = new JMenu("Hints");
+		hintMenu.setMnemonic( 'n' );
+		
+		JMenuItem checkOnFill = new JMenuItem( "Toggle Check on Fill mode" );
+	    checkOnFill.setMnemonic( 'C' );
+	    hintMenu.add( checkOnFill );
+	    checkOnFill.addActionListener(
+
+	    		new ActionListener() {
+
+	    			public void actionPerformed( ActionEvent event )
+	    			{
+	    				//Check on Fill toggle to be placed here
+	    			}
+	    		}
+	    );
+	    
+	    JMenuItem sAlgorithm = new JMenuItem( "Use Single Algorithm" );
+	    sAlgorithm.setMnemonic( 'S' );
+	    hintMenu.add( sAlgorithm );
+	    sAlgorithm.addActionListener(
+
+	    		new ActionListener() {
+
+	    			public void actionPerformed( ActionEvent event )
+	    			{
+	    				//Single algorithm action to be placed here
+	    			}
+	    		}
+	    );
+	    
+	    JMenuItem hAlgorithm = new JMenuItem( "Use Hidden Single Algorithm" );
+	    hAlgorithm.setMnemonic( 'H' );
+	    hintMenu.add( hAlgorithm );
+	    hAlgorithm.addActionListener(
+
+	    		new ActionListener() {
+
+	    			public void actionPerformed( ActionEvent event )
+	    			{
+	    				//Hidden Single algorithm action to be placed here
+	    			}
+	    		}
+	    );
+	    
+	    JMenuItem lAlgorithm = new JMenuItem( "Use Locked Candidate Algorithm" );
+	    lAlgorithm.setMnemonic( 'L' );
+	    hintMenu.add( lAlgorithm );
+	    lAlgorithm.addActionListener(
+
+	    		new ActionListener() {
+
+	    			public void actionPerformed( ActionEvent event )
+	    			{
+	    				//Locked Candidate algorithm action to be placed here
+	    			}
+	    		}
+	    );
+	    
+	    JMenuItem nAlgorithm = new JMenuItem( "Use Naked Pairs Algorithm" );
+	    nAlgorithm.setMnemonic( 'N' );
+	    hintMenu.add( nAlgorithm );
+	    nAlgorithm.addActionListener(
+
+	    		new ActionListener() {
+
+	    			public void actionPerformed( ActionEvent event )
+	    			{
+	    				//Naked Pairs algorithm action to be placed here
+	    			}
+	    		}
+	    );
+	    
+	    JMenuItem useAll = new JMenuItem( "Fill in all!" );
+	    useAll.setMnemonic( 'a' );
+	    hintMenu.add( useAll );
+	    useAll.addActionListener(
+
+	    		new ActionListener() {
+
+	    			public void actionPerformed( ActionEvent event )
+	    			{
+	    				//all 4 algorithm actions placed here
+	    			}
+	    		}
+	    );
+	    
+	    JMenuBar bar = new JMenuBar();  
+	    setJMenuBar( bar );  
+	    bar.add( fileMenu );
+	    bar.add( helpMenu );
+	    bar.add( hintMenu );
+	    
+//	    JLabel displayLabel = new JLabel( "Sample Text", SwingConstants.CENTER );
+//	      displayLabel.setForeground( Color.BLACK );
+//	      displayLabel.setFont( new Font( "Serif", Font.PLAIN, 72 ) );
+
+	 
+
+	      setSize( 600, 600 );
+	      setVisible( true );
+		
+		
+		
+	}
+
+	
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		if(toggle){
+			
+		}
+		toggle = !toggle;
+		smallContainer.validate();
+		
+	//	MyJButton temp = ()
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SudokuGUI application = new SudokuGUI();
+	    application.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+	}
+	
+	
+
+}
+
+class MyJPanel extends JPanel
+{
+	int region;
+	public MyJPanel(GridLayout g, int r){
+		super(g);
+		region = r;
+	}
+	public int getRegion(){
+		return this.region;
+	}
+	public void setRegion(int r){
+		region = r;
+	}
+	
+}
+
+
+class MyJButton extends JButton
+{
+  private int num;
+  private int x;
+  private int y;
+  public MyJButton ( String text )
+  {
+    super (text);
+    //setText (text);
+  }
+  
+  public MyJButton (String text, int a, int b){
+	  super(text);
+	  x = a;
+	  y = b;
+  }
+  
+  public MyJButton ( String text , int n)
+  {
+    super (text);
+    num = n;
+  }
+  
+  public void setNumber (int n)
+  {
+    num = n;
+  }
+  
+  public int getNumber ()
+  {
+    return num;
+  }
+  public void setCoordintes(int a, int b){
+	  this.x = a;
+	  this.y = b;
+  }
+  
+  public int getXCoordinate(){
+	  return this.x;
+  }
+  
+  public int getYCoordinate(){
+	  return this.y;
+  }
+}
+
